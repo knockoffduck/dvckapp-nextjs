58632abeac8ed0b78a87a2c43b9a3b2fb667e5d8
knockoffduck <ddarm4@gmail.com>
2022-11-03 16:22:39 +0800

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (<div>
     <header className='flex items-center absolute w-screen h-20 p-4'>
-      <a href="/" className='font-bold text-4xl z-10' >dvck</a>
+      <Link href="/" className='font-bold text-4xl z-10' >dvck</Link>
 
     </header>
     <Component {...pageProps} />
