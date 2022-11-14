998c8939b0d12c30d8581e38698d52dab92c8382
knockoffduck <ddarm4@gmail.com>
2022-11-15 02:03:29 +0800

    finshed

--- pages/Coles.jsx
+++ pages/Coles.jsx
@@ -29,11 +29,11 @@ export default function Coles() {
     }
 
     return (
-        <div className='grid bg-[#191c21] h-full w-screen gap-6 place-items-center'>
+        <div className={fetched ? 'grid bg-[#191c21] h-full w-screen gap-6 place-items-center' : 'grid bg-[#191c21] h-screen w-screen gap-6'}>
             <div className='h-[70px] w-full p-6'>
                 <h1 className='text-4xl font-bold'>Shift Details</h1>
             </div>
-            {!fetched ? <button onClick={() => getShiftData()} className={!loading ? 'btn w-[150px]' : 'btn loading'}>{!loading ? 'Get Data' : 'Fetching Data'}</button> :
+            {!fetched ? <div className='flex justify-center'><button onClick={() => getShiftData()} className={!loading ? 'btn w-[150px]' : 'btn loading'}>{!loading ? 'Get Data' : 'Fetching Data'}</button></div> :
                 <div className='grid grid-cols-1 place-items-center pb-14'>
                     <div className='flex flex-col bg-[#24292d] drop-shadow-2xl w-[332px] h-[664px] rounded-lg'>
                         <div className='grid place-items-center h-[70px] w-full pt-3'>
@@ -65,30 +65,32 @@ export default function Coles() {
                     </div>
                 </div>
             }
-            <div className='flex flex-col bg-[#24292d] drop-shadow-2xl w-full rounded-xl'>
-                <div className='grid h-[70px] w-full p-6'>
-                    <h2 className='text-2xl'>This Week&apos;s Shifts</h2>
-                </div>
-                <div className="grid px-6 pb-6 gap-8">
-                    {fetched && data.ShiftDetails.map((shift, index) => {
-                        return (
-                            <div key={index} className='grid grid-cols-3 grid-rows-1 bg-[#24292d] drop-shadow-2xl w-full h-[100px] rounded-xl justify-between'>
-                                <div className='flex flex-col justify-center pl-6 w-[1/3]'>
-                                    <h3 className=''>{moment(shift.day, 'ddd').format('ddd')} {moment.localeData().ordinal(shift.date)}</h3>
-                                    <h4 className='text-white text-thin text-opacity-50 text-sm'>{shift.totalHours} Hours</h4>
+            {fetched &&
+                <div className='flex flex-col bg-[#24292d] drop-shadow-2xl w-full rounded-xl'>
+                    <div className='grid h-[70px] w-full p-6'>
+                        <h2 className='text-2xl'>This Week&apos;s Shifts</h2>
+                    </div>
+                    <div className="grid px-6 pb-6 gap-8">
+                        {fetched && data.ShiftDetails.map((shift, index) => {
+                            return (
+                                <div key={index} className='grid grid-cols-3 grid-rows-1 bg-[#24292d] drop-shadow-2xl w-full h-[100px] rounded-xl justify-between'>
+                                    <div className='flex flex-col justify-center pl-6 w-[1/3]'>
+                                        <h3 className=''>{moment(shift.day, 'ddd').format('ddd')} {moment.localeData().ordinal(shift.date)}</h3>
+                                        <h4 className='text-white text-thin text-opacity-50 text-sm'>{shift.totalHours} Hours</h4>
+                                    </div>
+                                    <div className='flex flex-col justify-center w-[1/3]'>
+                                        <h3 className='text-white text-opacity-50 text-sm text-center'>{shift.start} - {shift.end}</h3>
+                                    </div>
+                                    <div className='flex flex-col justify-center px-6 w-[1/3]'>
+                                        <h3 className='text-xl font-bold text-center'>${shift.pay.toFixed(2)}</h3>
+                                    </div>
                                 </div>
-                                <div className='flex flex-col justify-center w-[1/3]'>
-                                    <h3 className='text-white text-opacity-50 text-sm text-center'>{shift.start} - {shift.end}</h3>
-                                </div>
-                                <div className='flex flex-col justify-center px-6 w-[1/3]'>
-                                    <h3 className='text-xl font-bold text-center'>${shift.pay.toFixed(2)}</h3>
-                                </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
 
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
