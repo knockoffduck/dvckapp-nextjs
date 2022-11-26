713d7aa5dae5f04a759bb109fe1120ac5be341fb
knockoffduck <ddarm4@gmail.com>
2022-11-27 03:00:43 +0800

    test

--- components/ShiftViewer.js
+++ components/ShiftViewer.js
@@ -2,6 +2,8 @@ import { format, parse } from 'date-fns';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import io from "socket.io-client";
+
 
 async function fetchProducts(url) {
     const response = await fetch(url, {
@@ -17,31 +19,69 @@ async function fetchProducts(url) {
 }
 
 export default function ShiftViewer() {
-    const [current, setCurrrent] = useState(0)
+    const [current, setCurrent] = useState(0)
+    const [week, setWeek] = useState(null)
     const [loading, setLoading] = useState(false)
     const [fetched, setFetched] = useState(false)
     const [data, setData] = useState([])
+    const [currentShifts, setCurrentShifts] = useState(null)
+    const [shiftData, setShiftData] = useState([])
     let [length, setLength] = useState(0);
+    const [updated, setUpdated] = useState('')
+    const [socket, setSocket] = useState(null)
+    const [progress, setProgress] = useState(0)
+    const [pressed, setPressed] = useState(false)
     const url = 'https://dvckapp.xyz/api';
 
     const nextSlide = () => {
-        setCurrrent(current === length - 1 ? 0 : current + 1)
+        setCurrent(current === length - 1 ? 0 : current + 1)
+        setWeek(data[current])
+        console.log(data[current].date)
+        setPressed(false)
     }
     const prevSlide = () => {
-        setCurrrent(current === 0 ? length - 1 : current - 1)
+        setCurrent(current === 0 ? length - 1 : current - 1)
+        setWeek(data[current])
+        console.log(data[current].date)
+        setPressed(false)
+    }
+    const test = () => {
+        updateShifts()
+        setPressed(true)
     }
 
+    const updateShifts = () => {
+        const shiftArray = []
+        shiftData.map((shift) => {
+            if (shift.WeekID == data[current].id) {
+                shiftArray.push(shift)
+            }
+        })
+        setCurrentShifts(shiftArray)
+        console.log(data[current], currentShifts)
+    }
     const loadData = async () => {
         setLoading(true)
         try {
             const data = await fetchProducts(url)
+            const shiftdata = await fetchProducts('https://dvckapp.xyz/shifts')
+            setShiftData(shiftdata)
             setData(data.reverse())
             setLength(await data.length)
             setLoading(false)
+            setWeek(data[current])
             setFetched(true)
         } catch (error) {
             console.log(error.message)
         }
+        if (fetched) {
+        }
+    }
+    const sendReq = () => {
+        socket.emit(`request', 'begin, ${data[current].timetable_url}`)
+    }
+    const sendCount = () => {
+        socket.emit('request', 'checkCount')
     }
 
     useEffect(() => {
@@ -49,6 +89,30 @@ export default function ShiftViewer() {
             loadData()
         }
     }, []);
+
+    useEffect(() => {
+        if (!socket) {
+            setSocket(io("https://dvckapp.xyz", { path: '/ws' }))
+        }
+        return () => {
+            socket?.disconnect()
+        }
+    }, [socket])
+
+    useEffect(() => {
+        if (!socket) return
+        socket.on('message', (text) => {
+            setUpdated(text)
+        })
+        socket.on('progress', (percentage) => {
+            setProgress(parseFloat(percentage))
+        })
+
+        return () => {
+            socket.off('message')
+            socket.off('progress')
+        }
+    }, [socket])
 
 
 
@@ -60,29 +124,34 @@ export default function ShiftViewer() {
                 </h1>
             </div>
             <div className='flex flex-col w-full pb-8 gap-4 justify-center place-items-center'>
-                <div className="bg-[url('../public/ShiftBG.svg')] h-[72px] w-[320px] flex text-black">
-                    <div className='text-center  flex flex-col  justify-center w-[80px]'>
-                        <h5 className='font-bold leading-6 text-2xl'>Thu</h5>
-                        <h5 className='text-md leading-6'>10 Nov</h5>
-                    </div>
-                    <div className='flex w-[150px] justify-center items-center'>
-                        <h5 className='text-md leading-6'>09:00 - 17:00</h5>
-                    </div>
-                    <div className='flex w-[100px] justify-center items-center'>
-                        <h5 className='font-bold text-xl'>8 Hrs</h5>
-                    </div>
-                </div>
+                {pressed &&
+                    currentShifts.map((shift, index) => {
+                        return (
+                            <div key={index} className="bg-[url('../public/ShiftBG.svg')] h-[72px] w-[320px] flex text-black">
+                                <div className='text-center  flex flex-col  justify-center w-[80px]'>
+                                    <h5 className='font-bold leading-6 text-2xl'>{shift.Day}</h5>
+                                    <h5 className='text-md leading-6'>{shift.Date}</h5>
+                                </div>
+                                <div className='flex flex-col w-[150px] justify-center items-center'>
+                                    <h5 className='text-sm leading-6'>{shift.Start} to {shift.End}</h5>
+                                </div>
+                                <div className='flex w-[100px] justify-center items-center'>
+                                    <h5 className='font-bold text-xl'>{shift.Hours} Hrs</h5>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
 
             </div>
             <div className='bg-[#25292D] flex flex-col pt-4 h-[670px] w-screen rounded-t-2xl'>
-                <div className='border flex w-full h-[34px] col-span-2 place-items-center justify-center'>
-                    {fetched && data.map((timetable, index) => {
-                        if (index === current) {
-                            return (<h4 key={index} className='text-center text-2xl font-semibold'>{format(parse(timetable.date, 'dd MMMM yyyy', new Date), 'dd MMM')}</h4>)
-                        }
-                    }
-                    )
-                    }
+                <div className='flex w-full h-[34px] col-span-2 place-items-center justify-center'>
+                    {fetched && <h4 className='text-center text-2xl font-semibold'>{
+                        format(parse(data[current].date, 'dd MMMM yyyy', new Date), 'dd MMM')
+
+                    }</h4>}
+
+
                 </div>
                 <div className='grid grid-cols-2 grid-rows-2 py-5 px-7 gap-6 w-full'>
                     <div className='bg-[#393D42] text-white btn flex justify-center items-center shadow-md w-full h-[80px] rounded-lg'>
@@ -91,28 +160,19 @@ export default function ShiftViewer() {
                     <div className='bg-[#393D42] text-white btn flex justify-center items-center shadow-md w-full h-[80px] rounded-lg'>
                         <IoIosArrowForward onClick={prevSlide} className='text-5xl' />
                     </div>
-                    <div className='bg-[#7C7AFF] text-white btn normal-case shadow-md col-span-2 w-full h-[80px] rounded-lg place-items-center justify-center flex'>
-                        <h4 className='text-center text-2xl font-semibold'>
-                            Add to Calendar
-                        </h4>
+                    <div onClick={test} className='bg-[#7C7AFF] text-white btn normal-case shadow-md col-span-2 w-full h-[80px] rounded-lg place-items-center justify-center flex'>
+                        <h4 className='text-center text-2xl font-semibold'>Get Shifts</h4>
                     </div>
                 </div>
                 <div className='grid w-full h-[34px] col-span-2 place-items-center justify-center pt-10 gap-10'>
                     <h4 className='text-center text-2xl font-semibold'>Timetable</h4>
-                    <div className=''>
+                    <div className='grid gap-6 justify-items-center' >
                         {fetched &&
-                            data.map((timetable, index) => {
-                                return (<div key={index} className='grid gap-6 justify-items-center' >
-                                    {index === current && (
-                                        <div className="relative h-[179px] w-[316px]">
-                                            <Image src={timetable.timetable_url} fill alt={timetable.date} priority className="rounded-lg" />
-                                        </div>
-                                    )}
-
-                                </div>
-                                )
-                            })
+                            <div className="relative h-[179px] w-[316px]">
+                                <Image src={data[current].timetable_url} fill alt={data[current].date} priority className="rounded-lg" />
+                            </div>
                         }
+
                     </div>
                 </div>
             </div>
--- pages/Test.jsx
+++ pages/Test.jsx
@@ -1,20 +1,16 @@
 import ProgressBar from "@ramonak/react-progress-bar";
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from "socket.io-client";
-const ENDPOINT = "http://localhost:3639"
+const ENDPOINT = "https://dvckapp.xyz"
 
 export default function Test() {
-
-    const [response, setResponse] = useState("")
-    const inputRef = useRef(null)
     const [updated, setUpdated] = useState('')
     const [socket, setSocket] = useState(null)
-    const [users, setUsers] = useState(0)
     const [progress, setProgress] = useState(0)
 
     useEffect(() => {
         if (!socket) {
-            setSocket(io(ENDPOINT))
+            setSocket(io(ENDPOINT, { path: '/ws' }))
         }
         return () => {
             socket?.disconnect()
@@ -33,17 +29,11 @@ export default function Test() {
         return () => {
             socket.off('message')
             socket.off('progress')
-
         }
     }, [socket])
 
-    const sendMessage = () => {
-        setUpdated(inputRef.current.value)
-
-    }
-
     const sendReq = () => {
-        socket.emit('request', 'begin,https://nextlevel.worldmanager.com/content/forum_uploads/files/2247/Roster_W_E_12_10_22.jpg')
+        socket.emit('request', 'begin, https://nextlevel.worldmanager.com/content/forum_uploads/files/2346/Roster_W_E_07_12_2022.jpg')
     }
     const sendCount = () => {
         socket.emit('request', 'checkCount')
@@ -54,11 +44,6 @@ export default function Test() {
             <div className='grid w-full place-items-center'>
                 <h3>{updated}</h3>
                 <ProgressBar customLabel=" " className="w-[50%]" completed={progress} />
-            </div>
-            <div className='flex gap-4'>
-                <input ref={inputRef} type="text" placeholder='Type Here' className="input w-full max-w-xs" />
-                <button onClick={sendMessage} className='btn'> submit</button>
-
             </div>
             <div className='flex gap-4'>
                 <button onClick={sendReq} className='btn'>Request</button>
